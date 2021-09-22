abf7a404321b5d03617bbf6cf3ce000c7f7eb2e4
Adam Zerner <adamzerner@protonmail.com>
2021-09-21 19:20:05 -0700

    Placeholders for BetSizeButtons

--- src/BetSizeButtons.js
+++ src/BetSizeButtons.js
@@ -1,7 +1,16 @@
+import './BetSizeButtons.css'
+import Button from 'react-bootstrap/Button';
+
 function BetSizeButtons() {
   return (
-    <section>
-      bet size buttons placeholder
+    <section class="bet-size-buttons">
+      <Button variant="outline-secondary">20%</Button>
+      <Button variant="outline-secondary">25%</Button>
+      <Button variant="outline-secondary">33%</Button>
+      <Button variant="outline-secondary">50%</Button>
+      <Button variant="outline-secondary">67%</Button>
+      <Button variant="outline-secondary">75%</Button>
+      <Button variant="outline-secondary">100%</Button>
     </section>
   )
 }
